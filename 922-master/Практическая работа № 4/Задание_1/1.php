633f01964dfa8bf3cfd613df49cb7f7d2c73a3ad
Torgheaven <torgmosos@gmail.com>
2026-05-05 09:55:01 +0600

    Пр4 1 задание

--- 922-master/Практическая работа № 4/Задание_1/1.php
+++ 922-master/Практическая работа № 4/Задание_1/1.php
@@ -6,31 +6,46 @@
     <title>Document</title>
 </head>
 <body>
-    <form action="" method="post">
+    <form action="" method="POST">
         <input type="number" name="a"><br><br>
         <input type="number" name="b"><br><br>
         <input type="number" name="c"><br><br>
         <input type="submit" value="Отправить">
     </form>
     
-    <?php
-            if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $a = $_post['a'];
-        $b = $_post['b'];
-        $c = $_post['c'];
-            }
-       
-    if ($a && $b && $c){
-        $q=0;
-    }
-    elseif (($a && $b)||($b && $c)||($a && $c)){
-        $q = 0;
-    }
-    elseif ($a || $b || $c){
-        $q = 1;
-    }
-    ?>
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $a = $_POST["a"];
+    $b = $_POST["b"];
+    $c = $_POST["c"];
     
+        
+       
+
+        } 
+
+
+
+ 
+        if ($a && $b && $c ) {
+            $q = 0;
+        } elseif (($a && $b )|| ($b && $c) || ($a && $a)) {
+            $q = 1;
+        } elseif ($a || $b ||$c ) {
+            $q = 1;
+    
+        }
+         else { 
+            $q = 0;
+        }
+    
+    
+
+            echo "<table border='1'><tr><th>Ввод</th><th>Вывод</th></tr><br>";
+                echo"<tr><td>a=$a, b=$b, c=$c</td><td>q=$q</td></tr></table>";
+
+    ?>
+   
 </body>
 
 </html>
