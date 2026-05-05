749225d50f2d283edc362415dc4cd30e9f6d6c4d
Torgheaven <torgmosos@gmail.com>
2026-05-05 08:51:12 +0600

    7 задание

--- 922-master/Практическая работа № 3/Задание_7/7.php
+++ 922-master/Практическая работа № 3/Задание_7/7.php
@@ -8,87 +8,87 @@
 <body>
 
     <?php
-    $discography = array(
-		array("id" => ["1"],
+    $discography = [
+		["id" => ["1"],
 		"name" => ["Atom Heart Mother"],
 		"date" => ["10 октября 1970"],
 		"label" => ["EMI, Harvest, Capitol"],
 		"format" => ["LP, CD"],
 		"status" => ["Золотой (USA)"]
-        ),
+	],
 
-		array("id" => ["2"],
+		["id" => ["2"],
 		"name" => ["Meddle"],
 		"date" => ["30 октября 1971"],
 		"label" => ["EMI, Harvest, Capitol"],
 		"format" => ["Vinyl, Кассета, CD"],
 		"status" => ["Платиновый (USA)"]
-        ),
+		],
 
-		array("id" => ["3"],
+		["id" => ["3"],
 		"name" => ["Obscured by Clouds"],
 		"date" => ["3 июня 1972"],
 		"label" => ["EMI, Harvest, Capitol"],
 		"format" => ["LP, Кассета, CD"],
 		"status" => ["Золотой (USA)", "Серебряный (GBR)"]
-        ),
+	],
 
-		array("id" => ["4"],
+		["id" => ["4"],
 		"name" => ["The Dark Side of the Moon"],
 		"date" => ["17 марта 1973"],
 		"label" => ["Harvest, Capitol, EMI"],
 		"format" => ["LP, Кассета, CD, SACD"],
 		"status" => ["Платиновый (USA), Платиновый (GBR), Бриллиантовый (CAN)"]
-        ),
+],
 
-		array("id" => ["5"],
+		["id" => ["5"],
 		"name" => ["Wish You Were Here"],
 		"date" => ["15 сентября 1975"],
 		"label" => ["Harvest, EMI, Columbia, Capitol"],
 		"format" => ["LP, 8-track, Кассета, CD, SACD"],
 		"status" => ["Платиновый (USA), Золотой (GBR), Платиновый (CAN)"]
-        ),
+],
 
-		array("id" => ["6"],
+		["id" => ["6"],
 		"name" => ["Animals"],
 		"date" => ["23 января 1977"],
 		"label" => ["Harvest, EMI Columbia, Capitol"],
 		"format" => ["LP, 8-track, Кассета, CD"],
 		"status" => ["Платиновый (USA), Золотой (GBR), Платиновый (CAN)"]
-        ),
+],
 
-		array("id" => ["7"],
+		["id" => ["7"],
 		"name" => ["The Wall"],
 		"date" => ["30 ноября 1979"],
 		"label" => ["Harvest, EMI Columbia, Capitol"],
 		"format" => ["LP, 8-track, Кассета, CD"],
 		"status" => ["Платиновый (USA), Платиновый (GBR), Бриллиантовый (CAN), Платиновый (NLD)"]
-        ),
+],
 
-		array("id" => ["8"],
+		["id" => ["8"],
 		"name" => ["The Final Cut"],
 		"date" => ["21 марта 1983"],
 		"label" => ["Harvest, EMI Columbia, Capitol"],
 		"format" => ["LP, 8-track, Кассета, CD"],
 		"status" => ["Платиновый (USA), Золотой (GBR), Золотой(NLD)"]
-        ),
+],
 
-		array("id" => ["9"],
+		["id" => ["9"],
 		"name" => ["A Momentary Lapse of Reason"],
 		"date" => ["8 сентября 1987"],
 		"label" => ["EMI, Columbia"],
 		"format" => ["LP, Кассета, CD"],
 		"status" => ["Платиновый (USA), Золотой (GBR), Платиновый (CAN), Золотой(NLD)"]
-        ),
+],
 		
-		array("id" => ["10"],
+		["id" => ["10"],
 		"name" => ["The Division Bell"],
 		"date" => ["30 марта 1994"],
 		"label" => ["EMI, Columbia"],
 		"format" => ["LP, Кассета, CD"],
 		"status" => ["Платиновый (USA), Платиновый (GBR), Платиновый (CAN), Платиновый(NLD)"]
-        )
-    );
+		]
+];
 ?>
 <table border="1">
     <tr>
@@ -101,12 +101,12 @@
     </tr>
     <?php foreach ($discography as $discographys): ?>
         <tr>
-            <td><?php echo $discographys['id']; ?></td>
-            <td><?php echo $discographys['name']; ?></td>
-			<td><?php echo $discographys['date']; ?></td>
-            <td><?php echo $discographys['label']; ?></td>
-			<td><?php echo $discographys['format']; ?></td>
-            <td><?php echo $discographys['status']; ?></td>
+            <td><?php print_r ($discographys['id'][0]); ?></td>
+            <td><?php print_r ($discographys['name'][0]); ?></td>
+			<td><?php print_r ($discographys['date'][0]); ?></td>
+            <td><?php print_r ($discographys['label'][0]); ?></td>
+			<td><?php print_r ($discographys['format'][0]); ?></td>
+            <td><?php print_r ($discographys['status'][0]); ?></td>
         </tr>
     <?php endforeach; ?>
 </table>
