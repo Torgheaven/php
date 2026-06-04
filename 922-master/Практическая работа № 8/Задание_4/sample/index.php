de9de4abcec8db8277e8821da35b7858caaf07f0
Torgheaven <torgmosos@gmail.com>
2026-06-04 13:16:01 +0600

    пр 8 4-5 задания

--- 922-master/Практическая работа № 8/Задание_4/sample/index.php
+++ 922-master/Практическая работа № 8/Задание_4/sample/index.php
@@ -9,19 +9,50 @@
 	<h1>Отправка данных на сервер</h1>
 	<h2>Отправка форм</h2>
 	
-	<?php
-		/* 
-		загружая форму, не забудьте передать GET-параметр view ...
-	
+	<?php	
+
+// проверяем массив $_GET 
+echo "<pre><b>Данные массива GET</b><br />"; 
+print_r($_GET); 
+echo "</pre>"; 
+// проверяем массив $_POST 
+echo "<pre><b>Данные массива POST</b><br />"; 
+print_r($_POST); 
+echo "</pre>"; 
+if(isset($_GET['data']['role']) && 
+isset($_GET['data']['action']) && isset($_GET['mood'])){ 
+// выводим полученные от пользователя данные (массив GET) 
+echo <<<HERE
+<h2>Полученные от пользователя данные массива 
+                                          
+                           
+                                             
+GET:</h2> 
+Роль: {$_GET['data']['role']} <br /> 
+Действие: {$_GET['data']['action']} <br /> 
+Настроение: {$_GET['mood']} 
+HERE; 
+} 
+if(isset($_POST['login']) && isset($_POST['email']) && 
+isset($_POST['pwd'])){ 
+// выводим полученные от пользователя данные (массив POST) 
+echo <<<HERE
+<h2>Полученные от пользователя данные массива 
+POST:</h2> 
+Логин: {$_POST['login']} <br /> 
+E-mail: {$_POST['email']} <br /> 
+Пароль: {$_POST['pwd']} 
+HERE; 
+}
 		
-		?view=dump  -> отобразим данные формы объектом PHP
-		?view=json  -> отобразим данные формы объектом JSON
-		*/
+		//?view=dump  -> $_GET["post"]
+		//?view=json  -> отобразим данные формы объектом JSON
+		//*/
 
 		// ...
 	?>
 
-	<form action="server.php?view=<?php // GET-параметр  ?>" method="post">
+	<form action="server.php?view=<?php echo htmlspecialchars($_GET['view'] ?? '') ?>" method="post">
 		Название: <input type="text" name="name"><p>
 		Алиас: <input type="text" name="alias"><p>
 		Страна: <input type="text" name="country"><p>
