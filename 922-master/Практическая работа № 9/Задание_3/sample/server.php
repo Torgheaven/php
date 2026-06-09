fee6ff8c07778454a30b122ceac3234f82cf76de
Torgheaven <torgmosos@gmail.com>
2026-06-09 12:22:09 +0600

    исправления

--- 922-master/Практическая работа № 9/Задание_3/sample/server.php
+++ 922-master/Практическая работа № 9/Задание_3/sample/server.php
@@ -1,73 +1,46 @@
-<?php
-if (isset($_POST['loader'])) {
-	echo "<h2>На сервере приняты данные форм</h2>";
-	printf ("
-	<i>Фамилия: </i> <b>%s</b>, <p>
-	<i>Имя: </i> <b>%s</b>, <p>
-	<i>Отчество: </i> <b>%s</b>, <p>
-	<i>Должность: </i> <b>%s</b>, <p>
-	<i>Категория: </i> <b>%s</b>, <p>
-    <i>Стаж в техникуме: </i> <b>%s</b>
-	",
-		$_POST['surname'],
-		$_POST['name'],
-		$_POST['patronymic'],
-		$_POST['post'],
-		$_POST['category'],
-        $_POST['experience_college']); 
-	
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    echo "<h1>На сервер приняты данные формы</h1>";
+    if ($_POST['surname'] != "") {
+        echo "<p><i>Фамилия: </i><b>{$_POST['surname']}</b>,</p>";
+    }
+    if ($_POST['name'] != "") {
+        echo "<p><i>Имя: </i><b>{$_POST['name']}</b>,</p>";
+    }
+    if ($_POST['patronymic'] != "") {
+        echo "<p><i>Отчество: </i><b>{$_POST['patronymic']}</b>,</p>";
+    }
+    if ($_POST['post'] != "") {
+        echo "<p><i>Должность: </i><b>{$_POST['post']}</b>,</p>";
+    }
+    if ($_POST['category'] != "") {
+        echo "<p><i>Категория: </i><b>{$_POST['category']}</b>,</p>";
+    }
+    if ($_POST['experience_college'] != "") {
+        echo "<p><i>Стаж в техникуме: </i><b>{$_POST['experience_college']}</b>.</p>";
+    }
 
-}
-// проверим, что скрипт выполняется как обработчик формы
-		if (isset ($_POST["loader"])) {
-			
-			// проверим загрузку на наличие ошибок
-			if ($_FILES['image']["error"] == UPLOAD_ERR_OK) {
+    echo "<h1>Пользователь пытается загрузить файл</h1>";
+    if ($_FILES['image']['name'] != "") {
+        echo "<p><i>Имя файла: </i><b>{$_FILES['image']['name']}</b>,</p>";
+        echo "<p><i>Размер файла: </i><b>{$_FILES['image']['size']}</b> байт,</p>";
+        echo "<p><i>Тип содержимого файла: </i><b>{$_FILES['image']['type']}</b>,</p>";
+        echo "<p><i>Имя временного файла: </i><b>{$_FILES['image']['tmp_name']}</b>,</p>";
+        echo "<p><i>Код ошибки: </i><b>{$_FILES['image']['error']}</b>.</p>";
+    } else {
+        echo "<p><i>Код ошибки: </i><b>{$_FILES['image']['error']}</b>.</p>";
+        echo "Пользователь не выбрал файл<br />";
+        echo '<img src="' . "images/no-photo.jpg" . '" alt="Описание картинки">';
+    }
 
-				// генерируем уникальное имя файла
-				$filename = md5(uniqid());
-				
-				// получаем расширение файла
-				$ext = pathinfo($_FILES["image"]["name"], PATHINFO_EXTENSION);
+    ?>
 
-				// текущая временная директория
-				$current_path = $_FILES["image"]["tmp_name"];
-				
-				// директория постоянного хранения
-				$new_path = __DIR__ . '/upload/' . $filename . "." . $ext;
-
-				// перемещаем файл
-				if (move_uploaded_file($current_path, $new_path)) {
-					// выводим сообщение об успешной загрузке файла изображения
-					echo "<h3>Файл успешно загружен на сервер</h3>";
-					
-				} else {
-					// если во время перемещения возникла ошибка
-					echo "<h3>К сожалению, при сохранении файла что то пошло не так...</h3>";
-				};      
-
-			} else {
-			   // если при загрузке произошла ошибка, выводим информацию о ней
-				switch ($_FILES['image']['error']) {
-			        case UPLOAD_ERR_INI_SIZE:
-			            echo "<h3>Размер принятого файла превысил максимально допустимый размер, который задан директивой upload_max_filesize конфигурационного файла php.ini (код ошибки: 1)</h3>";
-						break;
-			        case UPLOAD_ERR_PARTIAL:
-			            echo "<h3>Загружаемый файл был получен только частично (код ошибки: 3)</h3>";
-						break;
-			        case UPLOAD_ERR_NO_FILE:
-			        	echo "<h3>Файл не был загружен (код ошибки: 4)</h3>";
-			        	break;
-			        default: 
-			        	echo "<h3>Файл по какой-то причине не загружен... :((</h3>";
-				}
-			}			
-		} else {
-
-			echo "<h3>Заполните, пожалуйста, форму</h3>";
-		
-		};
-		$a = "upload/";
-		$b = $a . basename($_FILES["image"]["name"]);
-		echo '<img src="' . $b. '" alt="Загруженное изображение" style="max-width: 500px; height: auto;">'
-	?>
+</body>
+</html>
